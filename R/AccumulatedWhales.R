5425194538e6c67332b5e73a501d06a6659c2ae1
grantdadams <grantadams60091@gmail.com>
2021-05-17 20:49:58 -0700

    Update AccumulatedWhales.R

--- R/AccumulatedWhales.R
+++ R/AccumulatedWhales.R
@@ -20,14 +20,15 @@ accum_fun <- function(a = -77.41,
                       d = -0.0003323,
                       mu = 60,
                       sigma = sqrt(60),
-                      x = 320){
+                      x = 320,
+                      ReportList = FALSE){
 
     t <- 1:365 # Julian day
     W_t <- c(0, exp(a + b + c * t + d * t^2)) # Estimated number of whales on day t: W_0 = 0
     W_t[1:100] <- 0 # No Whales until April
     P_t <- dnorm(t, mu, sigma, FALSE) # Probability whale remains in area
     dW_t <- c(W_t[2:366] - W_t[1:365]) # delta w - 2:366 in the first term because W_t goes from day 0 to day 365
-    A_x <- c(0) # Accumulated number of whales A_0 = 0
+    A_x <- rep(0, 366) # Accumulated number of whales A_0 = 0
 
     # Loop through K
     # -- Should be as follows:
@@ -39,5 +40,9 @@ accum_fun <- function(a = -77.41,
         A_x[k+1] <- sum(dW_t[1:k]) + sum(P_t[1:k] * A_x[k:1])
     }
 
-    return(A_x[x+1])
+    if(ReportList){
+        return(list(A_x = A_x, W_t = W_t, dW_t = dW_t))
+    } else{
+        return(A_x[x+1])
+    }
 }
